043b5707aa760fa4c398069dbfd43c6f997f69f1
Suvlovee <suvloveshah@gmail.com>
2026-05-28 12:03:36 +0545

    instructions

--- lab1/Name.java
+++ lab1/Name.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Name{
 	public static void main(String... args){
-		Scanner ram= new Scanner(System.in);
+		Scanner ram = new Scanner(System.in);
 		System.out.println("Enter your name");
 		String name= ram.nextLine();
 		System.out.print(" Your  entered name is " + name);
